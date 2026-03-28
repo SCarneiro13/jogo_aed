ec40e19271919ff9bf531946d5ffbf63758b7228
Gabriel Ramos <gr090655@gmail.com>
2026-03-27 22:28:13 -0300

    Completando o sistema do Usuario

---
+++ src/fila.c
@@ -0,0 +1,35 @@
+#include "fila.h"
+
+void inicializaFila(tp_fila *f){
+    f->ini = f->fim = MAX-1;
+}
+
+int filaVazia(tp_fila *f){
+    if(f->ini == f->fim) return 1;
+    return 0;
+}
+
+int proximo(int pos){
+    if (pos == MAX-1) return 0;
+    return ++pos;
+}
+
+int filaCheia(tp_fila *f){
+    if(proximo(f->fim) == f->ini) return 1;
+    return 0; 
+}
+
+int insereFila(tp_fila *f, tp_jogador e){
+    if(filaCheia(f)) return 0;
+    f->fim = proximo(f->fim);
+    f->item[f->fim] = e;
+    return 1;
+}
+
+int removeFila(tp_fila *f, tp_jogador *e){
+    if(filaVazia(f)) return 0;
+    f->ini = proximo(f->ini);
+    *e = f->item[f->ini];
+    return 1;
+}
+
--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include "fila.h"
+#include "usuario.h"
 
 int main(){
+    int qtdJog;
+    tp_fila fila;
+    inicializaFila(&fila);
 
+     do {
+        printf("Quantos jogadores (2 a 4): ");
+        scanf("%d", &qtdJog);
+    } while(qtdJog < 2 || qtdJog > 4);
 
+    for(int i = 0; i < qtdJog; i++){
+        Jogador j;
+        
+        printf("\n---JOGADOR %d---\n", i+1);
+
+        cadastrarJogador(&j);
+        inicializarJogador(&j);
+
+        insereFila(&fila, j);
+
+    }
+    
+    tp_jogador temp;
+    int i = 0;
+    while(!filaVazia(&fila)){
+        removeFila(&fila, &temp);
+        printf("Jogador %d: %s\n", i+1, temp.nick);
+    }
     return 0;
 }
