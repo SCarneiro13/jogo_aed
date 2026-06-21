43321daf8dfff2c0fd14bab8f129ffb08427efcf
mateuspauperio <pauperio.mateus@gmail.com>
2026-06-21 11:50:29 -0300

    quests.h

---
+++ include/quests.h
@@ -0,0 +1,24 @@
+#ifndef QUESTS_H
+#define QUESTS_H
+#define MAX_PERGUNTAS 27
+#include "space.h"
+#include "user.h"
+// A pergunta deve ser relacionada com sua unidade e ter um nível de dificuldade.
+typedef struct{
+    int id; // Identificador único da pergunta (índice em banco_completo).
+    int unidade; // Unidade da pergunta.
+    int dificuldade; // Nivel de dificuldade (De 0 a 2).
+    char pergunta[500]; // A pergunta.
+    char alternativas[5][250]; // Alternativas -> 5 alternativas de ate 250 caracteres.
+    int resposta; // Resposta correta (Indice de alternativas, vai de 0 a 4).
+} tp_pergunta;
+// Um baralho que é basicamente uma fila de perguntas.
+typedef struct{
+    tp_pergunta pergunta[MAX_PERGUNTAS/3];
+    int topo;
+} tp_baralho;
+void embaralharPerguntas(tp_pergunta *baralho, int num_questions, tp_baralho *uni1, tp_baralho *uni2, tp_baralho *uni3); // Função para embaralhar.
+void preparandoPergunta(tp_jogador *jogador, tp_baralho *uni1, tp_baralho *uni2, tp_baralho *uni3, tp_pergunta *banco_completo, int nmr_questoes); // Função para preparar a pergunta.
+int fazerPergunta(tp_jogador *j, tp_baralho *uni, tp_pergunta *banco_completo, int nmr_questoes, int unidade); // Função para realizar a pergunta.
+void reporPerguntas(int unidade, tp_pergunta *banco_completo, int nmr_questoes, tp_baralho *uni_atual); // Função para repor as perguntas 
+#endif
