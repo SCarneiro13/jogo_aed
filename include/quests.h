5da51cca62e29223f2e26859c58f847c8a2bc1bf
Gabriel Ramos <gr090655@gmail.com>
2026-06-25 18:38:31 -0300

    adicionando alterações

--- include/quests.h
+++ include/quests.h
@@ -32,11 +32,11 @@ void embaralharPerguntas(tp_pergunta *baralho, int num_questions, tp_baralho *un
 
 void preparandoPergunta(tp_jogador *jogador, tp_baralho *uni1, tp_baralho *uni2, tp_baralho *uni3, tp_pergunta *banco_completo, int nmr_questoes); // Função para preparar a pergunta.
 
-int fazerPergunta(tp_baralho *uni, tp_pergunta *banco_completo, int nmr_questoes, int unidade); // Função para realizar a pergunta.
+int fazerPergunta(tp_baralho *uni, tp_pergunta *banco_completo, int nmr_questoes, int unidade, char *nome_jogador); // Função para realizar a pergunta.
 
 void reporPerguntas(int unidade, tp_pergunta *banco_completo, int nmr_questoes, tp_baralho *uni_atual); // Função para repor as perguntas 
 
 void salvarHistoricoResposta (char nomeJogador[], tp_pergunta p, int respJogador, char resultado[]); // Função para salvar o historico das respostas
 
-void gerarArquivoReferencia (tp_pergunta *banco_completo, int nmr_questoes) // Função para gerar o arquivo de referência das perguntas
+void gerarArquivoReferencia (tp_pergunta *banco_completo, int nmr_questoes); // Função para gerar o arquivo de referência das perguntas
 #endif
